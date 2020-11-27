17d54d311f6143caeb0babaf078f25481c88ae89
lzrlove <lizong_run@126.com>
2020-11-27 18:13:47 +0800

    初次提交

---
+++ src/com/lzr/study/HelloWorld.groovy
@@ -0,0 +1,15 @@
+package com.lzr.study
+
+//class HelloWorld {
+//    public static void main(String[]args){
+//        System.out.println("Hello World!")
+//    }
+//}
+
+//class HelloWorld {
+//    static void main(String[]args){
+//        System.out.println("Hello World!")
+//    }
+//}
+
+println("Hello World!")
---
+++ src/com/lzr/study/Variable.groovy
@@ -0,0 +1,30 @@
+package com.lzr.study
+
+//强类型 groovy中的基本数据类型都是以对象的形式存在的 强类型没有初始值可以正常使用
+//int x = 1
+//double y = 3.14
+//char ch = 'a'
+//boolean flag = true
+
+//class java.lang.Integer
+//class java.lang.Double
+//class java.lang.Character
+//class java.lang.Boolean
+
+
+//弱类型 弱类型没有初始值不可以正常使用
+def x =1  //java.lang.NullPointerException: Cannot get property 'class' on null object
+def y = 3.14
+def ch = 'a'
+def flag = true
+
+//class java.lang.Integer
+//class java.math.BigDecimal
+//class java.lang.String
+//class java.lang.Boolean
+
+println x.class
+println y.class
+println ch.class
+println flag.class
+
